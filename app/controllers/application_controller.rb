e787927a53cf99503d19a4830ed9729085750eda
Jordan Kline <jordan.kline@gmail.com>
2018-02-10 00:13:47 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-    binding.pry
+
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.save
     redirect '/users/home'
